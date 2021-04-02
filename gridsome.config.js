1d462287f9f3385d38119023edf0095c75c5ff7f
Walter Hurtado <walter.hurtado@lkmx.io>
2021-04-02 15:27:56 -0700

    - Added meta title

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: "Flare",
+    siteName: "Secret Network",
     plugins: [{
             use: "@gridsome/vue-remark",
             options: {
