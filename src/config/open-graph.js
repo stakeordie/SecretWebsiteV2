8843defa954af3b707d3bbc37285041c810483b4
Walter Hurtado <walter.hurtado@lkmx.io>
2021-04-27 17:01:05 -0700

    - OpenGraph configuration

--- src/config/open-graph.js
+++ src/config/open-graph.js
@@ -1,36 +1,38 @@
-export default [{
-        key: 'og:url',
-        property: 'og:url',
-        content: '',
+export default [
+    {
+      key: 'og:url',
+      property: 'og:url',
+      content: 'https://scrt.network/',
     },
     {
-        key: 'og:type',
-        property: 'og:type',
-        content: 'website',
+      key: 'og:type',
+      property: 'og:type',
+      content: 'website',
     },
     {
-        key: 'og:title',
-        property: 'og:title',
-        content: 'Flare',
+      key: 'og:title',
+      property: 'og:title',
+      content: 'Secret Network',
     },
     {
-        key: 'og:description',
-        property: 'og:description',
-        content: 'Flare is a design system for agile front-end software engineering. Helps to understand the user interface from a simple and structured standpoint.',
+      key: 'og:description',
+      property: 'og:description',
+      content: 'Secret Network is a blockchain-based, open-source protocol that lets anyone perform computations on encrypted data, bringing privacy to smart contracts and public blockchains.',
     },
     {
-        key: 'og:image',
-        property: 'og:image',
-        content: 'cover.jpg',
+      key: 'og:image',
+      property: 'og:image',
+      content: 'https://scrt.network/cover.png',
     },
     {
-        key: 'twitter:card',
-        property: 'twitter:card',
-        content: 'summary_large_image',
+      key: 'twitter:card',
+      property: 'twitter:card',
+      content: 'summary_large_image',
     },
     {
-        key: 'twitter:image',
-        property: 'twitter:image',
-        content: 'cover.jpg',
+      key: 'twitter:image',
+      property: 'twitter:image',
+      content: 'https://scrt.network/cover.png',
     },
-]
+  ]
+  
