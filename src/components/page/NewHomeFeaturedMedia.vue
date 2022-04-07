cd1482fb2d139e582ca40bc0a96d244e9e8cd9b7
Cristina Valenzuela <cristina.valenzuela@lkmx.io>
2022-04-07 15:22:07 -0700

    NewHome: old feature media cards style

--- src/components/page/NewHomeFeaturedMedia.vue
+++ src/components/page/NewHomeFeaturedMedia.vue
@@ -21,33 +21,33 @@
     <!-- Left card -->
     <block>
 
-      <new-card-media :class="`accent-${left.type}`" :url="left.link">
+      <card-media :class="`accent-${left.type}`" :url="left.link">
         <span class="type">{{ left.type }}</span>
         <h6>{{ left.title }}</h6>
         <img :src="left.cover_image.url" :alt="left.title">
-      </new-card-media>
+      </card-media>
 
     </block>
 
     <!-- Center card -->
     <block>
 
-      <new-card-media :class="`accent-${center.type}`" :url="center.link">
+      <card-media :class="`accent-${center.type}`" :url="center.link">
         <span class="type">{{ center.type }}</span>
         <h6>{{ center.title }}</h6>
         <img :src="center.cover_image.url" :alt="center.title">
-      </new-card-media>
+      </card-media>
 
     </block>
 
     <!-- Right card -->
     <block>
 
-      <new-card-media :class="`accent-${right.type}`" :url="right.link">
+      <card-media :class="`accent-${right.type}`" :url="right.link">
         <span class="type">{{ right.type }}</span>
         <h6>{{ right.title }}</h6>
         <img :src="right.cover_image.url" :alt="right.title">
-      </new-card-media>
+      </card-media>
 
     </block>
 
