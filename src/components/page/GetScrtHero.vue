6234935fd3af65a556f8b4f126d577cd94a37594
Cristina Valenzuela <cristina.valenzuela@lkmx.io>
2022-04-13 11:20:00 -0700

    GetScrtHero currency select button (still in progress)

--- src/components/page/GetScrtHero.vue
+++ src/components/page/GetScrtHero.vue
@@ -15,14 +15,20 @@
           <h3>SCRT price</h3>
         </div>
         <!-- <h1>{{ scrtData.usd }}</h1> -->
-        <h1>{{ selected === '' ? scrtData.usd : scrtData[selected] }} {{ selected === '' ? 'usd' : selected }}</h1>
-        <select v-model="selected">
-          <option disabled value="">Please select one</option>
-          <option>usd</option>
-          <option>cad</option>
-          <option>eur</option>
-          <option>mxn</option>
-        </select>
+        <div class="price-select">
+          <h1>{{ selected === '' ? scrtData.usd : scrtData[selected] }} {{ selected === '' ? 'usd' : selected }}</h1>
+          <div class="custom-select">
+            
+            <select id="brow" class="standard-select" v-model="selected">
+              <!-- <input list="brow"> -->
+              <option hidden >Please select one</option>
+              <option>usd</option>
+              <option>cad</option>
+              <option>eur</option>
+              <option>mxn</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -104,69 +110,104 @@ export default {
         color: var(--color-analog-tertiary-yellow);
         margin-bottom: 0;
       }
+    }
 
-      &__info {
-        display: grid;
-        gap: 16px;
-        align-self: center;
+    &__price-box {
+      height: 400px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      align-self: center;
+      align-content: center;
+      color: var(--color-neutral-dark-mode-01);
 
-        & h5 {
-          color: var(--color-analog-tertiary-yellow);
-          margin-bottom: 0;
+      @include respond-to("<=s") {
+        //order: 1;
+      }
+
+      .price-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-bottom: 10px;
+
+        & img {
+          width: 42px;
+          height: 42px;
+          background: #fff;
+          border-radius: 50%;
+          margin-right: 10px;
+
         }
 
-        & h1 {
-          margin-bottom: 0;
-          line-height: 60px;
-        }
-
-        &__price-box {
-          height: 400px;
+        .price-title {
           display: flex;
-          flex-direction: column;
           justify-content: center;
-          text-align: center;
-          align-self: center;
-          align-content: center;
-          color: var(--color-neutral-dark-mode-01);
+          align-items: center;
 
-          @include respond-to("<=s") {
-            //order: 1;
+          & h1 {
+            font-family: Montserrat;
+            font-weight: 600;
+            font-size: 54px;
+            margin-bottom: 0;
           }
+        }
 
-          .price-title {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-
-            & img {
-              width: 48px;
-              height: 48px;
-              background: #fff;
-              border-radius: 50%;
-              margin-right: 10px;
-            }
-
-            .price-title {
-              display: flex;
-              justify-content: center;
-              align-items: center;
-
-              & h1 {
-                font-family: Montserrat;
-                font-weight: 600;
-                font-size: 54px;
-                margin-bottom: 0;
-              }
-            }
-
-            & h3 {
-              margin-bottom: 0;
-            }
-          }
+        & h3 {
+          margin-bottom: 0;
         }
       }
+
+      .price-select{
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          & select{
+            background: transparent;
+            border: none;
+
+            &:focus{
+              outline: none;
+            }
+          }
+
+          & .custom-select{
+            height: 20px;
+            width: 20px;
+            align-self: center;
+            
+            position: relative;
+            top: -8px;
+            right: -6px;
+
+            &::after{
+              content: "";
+              display: table-cell;
+              text-align: center;
+
+              width: 20px;
+              height: 20px;
+              padding: 2px;
+              background-image: url(../../assets/get-scrt/icon-repeat-no-bg.svg);
+              background-repeat: no-repeat;
+              background-size: 15px;
+              background-position: center;
+              background-color: var(--color-neutral-dark-mode-02);
+              border-radius: 50%;
+              position: absolute;
+              top: 0;
+              right: 0;
+
+              pointer-events: none;
+            }
+          }
+      }
+
+      
     }
+
   }
 }
 </style>
