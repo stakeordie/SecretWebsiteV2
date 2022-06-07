07c72e72a70bd0aa39f1831cf411e775acbc9ef0
Cristina Valenzuela <cristina.valenzuela@lkmx.io>
2022-06-07 09:54:41 -0700

    international-communities-v2 page uploaded

--- src/components/CardGridNfts.vue
+++ src/components/CardGridNfts.vue
@@ -597,9 +597,10 @@ $accent-colors: ("validator", "developer", "fund", "wallet");
           max-width: 400px;
           margin: auto;
           background: var(--color-neutral-dark-mode-04);
-          background-image: url(../assets/icon-search.svg);
+          background-image: url(../assets/search-icon-gray.svg);
           background-repeat: no-repeat;
-          border-color: var(--color-neutral-dark-mode-01);
+          //border-color: var(--color-neutral-dark-mode-01);
+          border: none;
           background-position: 8px;
           padding-left: 32px;
 
