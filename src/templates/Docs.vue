160594990ef6abb49c5b2e731a21fe185ea10a3e
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-07-18 18:44:45 -0400

    deep in docs scss

--- src/templates/Docs.vue
+++ src/templates/Docs.vue
@@ -32,11 +32,11 @@ export default {
 @import "../sass/docs/config/_colors.scss";
 @import "../sass/docs/config/_mixins.scss";
 @import "../sass/docs/config/_functions.scss";
-::v-deep > p {
+:deep(p) {
   opacity: .8;
 }
 
-::v-deep > h2 {
+:deep(h2) {
   padding-top: 100px;
   margin-top: -80px;
 
@@ -45,7 +45,7 @@ export default {
   }
 }
 
-::v-deep > p > img {
+:deep(p) > img {
     max-width: 100%;
   }
 
