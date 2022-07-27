5f03702e5c5b0ebeab5c4b964cd278ee238df3e6
Walter Hurtado <walterhf@gmail.com>
2022-07-27 14:35:11 -0700

    dummy data to object

--- src/components/learn/LearnArticleColumnDouble.vue
+++ src/components/learn/LearnArticleColumnDouble.vue
@@ -1,144 +1,115 @@
 <template>
   <div class="learn-article-column-double">
-
-
     <!-- **DOUBLE PARAGRAPH -->
 
-      <div class="learn-article-column-double__paragraph">
-        <div class="learn-article-column__text">
-          <h3 class="learn-article-column__title">H3 Title</h3>
-          <p class="learn-article-column__paragraph">
-            Facilisi nunc sapien velit sollicitudin viverra enim nec. Ornare
-            blandit interdum quisque sed habitasse augue orci. Volutpat amet ut
-            nisi, in mauris sagittis scelerisque quis. Sit sed felis egestas
-            mollis risus aliquam vitae, eget adipiscing. At aenean lorem tellus
-            arcu id tincidunt. Mattis neque morbi eget eget ultrices sed
-            aliquet. Porttitor amet, urna et in venenatis. Tortor metus, non
-            pulvinar sit ut.
-            <br />
-                <br />
-                Massa lectus at aliquet mauris habitant elementum purus. Aliquam dui
-                diam fermentum libero magna suspendisse sed tristique mi. Praesent ut
-                ultricies dignissim mi nunc in sit ac. Aliquet purus posuere ac justo,
-                condimentum lorem. Risus sit aenean nisl nibh id dictum. Enim rhoncus
-                vehicula vel sodales velit. Enim sit sit non at nulla. Tellus ultrices
-                enim elit nulla faucibus. Mi nunc, leo commodo leo rhoncus  placerat.
-          </p>
-        </div>
-        <div class="learn-article-column__text">
-          <h3 class="learn-article-column__title">H3 Title</h3>
-          <p class="learn-article-column__paragraph">
-            Facilisi nunc sapien velit sollicitudin viverra enim nec. Ornare
-            blandit interdum quisque sed habitasse augue orci. Volutpat amet ut
-            nisi, in mauris sagittis scelerisque quis. Sit sed felis egestas
-            mollis risus aliquam vitae, eget adipiscing. At aenean lorem tellus
-            arcu id tincidunt. Mattis neque morbi eget eget ultrices sed
-            aliquet. Porttitor amet, urna et in venenatis. Tortor metus, non
-            pulvinar sit ut.
-            <br />
-                <br />
-                Massa lectus at aliquet mauris habitant elementum purus. Aliquam dui
-                diam fermentum libero magna suspendisse sed tristique mi. Praesent ut
-                ultricies dignissim mi nunc in sit ac. Aliquet purus posuere ac justo,
-                condimentum lorem. Risus sit aenean nisl nibh id dictum. Enim rhoncus
-                vehicula vel sodales velit. Enim sit sit non at nulla. Tellus ultrices
-                enim elit nulla faucibus. Mi nunc, leo commodo leo rhoncus  placerat.
-          </p>
-        </div>
-      </div>
-
+    <div class="learn-article-column-double__paragraph">
+      <vue-markdown>
+        {{data}}
+      </vue-markdown>
+    </div>
 
     <!-- ** PARAGRAPH + IMAGE-->
 
-      <div class="learn-article-column-double__paragraph-img">
-        <div class="learn-article-column__text">
-          <h3 class="learn-article-column__title">H3 Title</h3>
-          <p class="learn-article-column__paragraph">
-            Facilisi nunc sapien velit sollicitudin viverra enim nec. Ornare
-            blandit interdum quisque sed habitasse augue orci. Volutpat amet ut
-            nisi, in mauris sagittis scelerisque quis. Sit sed felis egestas
-            mollis risus aliquam vitae, eget adipiscing. At aenean lorem tellus
-            arcu id tincidunt. Mattis neque morbi eget eget ultrices sed
-            aliquet. Porttitor amet, urna et in venenatis. Tortor metus, non
-            pulvinar sit ut.
-          </p>
-        </div>
-
-        <block class="">
-            <learn-article-image> </learn-article-image>
-        </block>
+    <div class="learn-article-column-double__paragraph-img">
+      <div class="learn-article-column__text">
+        <h3 class="learn-article-column__title">H3 Title</h3>
+        <p class="learn-article-column__paragraph">
+          Facilisi nunc sapien velit sollicitudin viverra enim nec. Ornare
+          blandit interdum quisque sed habitasse augue orci. Volutpat amet ut
+          nisi, in mauris sagittis scelerisque quis. Sit sed felis egestas
+          mollis risus aliquam vitae, eget adipiscing. At aenean lorem tellus
+          arcu id tincidunt. Mattis neque morbi eget eget ultrices sed aliquet.
+          Porttitor amet, urna et in venenatis. Tortor metus, non pulvinar sit
+          ut.
+        </p>
       </div>
 
+      <block class="">
+        <learn-article-image> </learn-article-image>
+      </block>
+    </div>
 
-      <!-- ** IMAGE + PARAGRAPH -->
+    <!-- ** IMAGE + PARAGRAPH -->
 
-      <div class="learn-article-column-double__paragraph-img">
-        <block class="">
-            <learn-article-image> </learn-article-image>
-        </block>
-        <div class="learn-article-column__text">
-          <h3 class="learn-article-column__title">H3 Title</h3>
-          <p class="learn-article-column__paragraph">
-            Facilisi nunc sapien velit sollicitudin viverra enim nec. Ornare
-            blandit interdum quisque sed habitasse augue orci. Volutpat amet ut
-            nisi, in mauris sagittis scelerisque quis. Sit sed felis egestas
-            mollis risus aliquam vitae, eget adipiscing. At aenean lorem tellus
-            arcu id tincidunt. Mattis neque morbi eget eget ultrices sed
-            aliquet. Porttitor amet, urna et in venenatis. Tortor metus, non
-            pulvinar sit ut.
-          </p>
-        </div>
+    <div class="learn-article-column-double__paragraph-img">
+      <block class="">
+        <learn-article-image> </learn-article-image>
+      </block>
+      <div class="learn-article-column__text">
+        <h3 class="learn-article-column__title">H3 Title</h3>
+        <p class="learn-article-column__paragraph">
+          Facilisi nunc sapien velit sollicitudin viverra enim nec. Ornare
+          blandit interdum quisque sed habitasse augue orci. Volutpat amet ut
+          nisi, in mauris sagittis scelerisque quis. Sit sed felis egestas
+          mollis risus aliquam vitae, eget adipiscing. At aenean lorem tellus
+          arcu id tincidunt. Mattis neque morbi eget eget ultrices sed aliquet.
+          Porttitor amet, urna et in venenatis. Tortor metus, non pulvinar sit
+          ut.
+        </p>
       </div>
-
-
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      data: `### H3 Title
+
+Facilisi nunc sapien velit sollicitudin viverra enim nec. Ornare blandit interdum quisque sed habitasse augue orci. Volutpat amet ut nisi, in mauris sagittis scelerisque quis. Sit sed felis egestas mollis risus aliquam vitae, eget adipiscing. At aenean lorem tellus arcu id tincidunt. Mattis neque morbi eget eget ultrices sed aliquet. Porttitor amet, urna et in venenatis. Tortor metus, non pulvinar sit ut.  
+  
+Massa lectus at aliquet mauris habitant elementum purus. Aliquam dui diam fermentum libero magna suspendisse sed tristique mi. Praesent ut ultricies dignissim mi nunc in sit ac. Aliquet purus posuere ac justo, condimentum lorem. Risus sit aenean nisl nibh id dictum. Enim rhoncus vehicula vel sodales velit. Enim sit sit non at nulla. Tellus ultrices enim elit nulla faucibus. Mi nunc, leo commodo leo rhoncus placerat.
+
+### H3 Title
+
+Facilisi nunc sapien velit sollicitudin viverra enim nec. Ornare blandit interdum quisque sed habitasse augue orci. Volutpat amet ut nisi, in mauris sagittis scelerisque quis. Sit sed felis egestas mollis risus aliquam vitae, eget adipiscing. At aenean lorem tellus arcu id tincidunt. Mattis neque morbi eget eget ultrices sed aliquet. Porttitor amet, urna et in venenatis. Tortor metus, non pulvinar sit ut.  
+  
+Massa lectus at aliquet mauris habitant elementum purus. Aliquam dui diam fermentum libero magna suspendisse sed tristique mi. Praesent ut ultricies dignissim mi nunc in sit ac. Aliquet purus posuere ac justo, condimentum lorem. Risus sit aenean nisl nibh id dictum. Enim rhoncus vehicula vel sodales velit. Enim sit sit non at nulla. Tellus ultrices enim elit nulla faucibus. Mi nunc, leo commodo leo rhoncus placerat.`
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 @import "@lkmx/flare/src/functions/_respond-to.scss";
 
 .learn-article-column-double {
+  display: grid;
+  gap: 64px;
+
+  &__paragraph,
+  &__paragraph-img {
     display: grid;
-    gap: 64px;
+    gap: 26px;
+    // gap: 54px;
+    grid-template-columns: repeat(auto-fit, minmax(342px, 1fr));
 
-    &__paragraph,
-    &__paragraph-img{
-        display: grid;
-        gap: 26px;
-        // gap: 54px;
-        grid-template-columns: repeat(auto-fit, minmax(342px, 1fr));
-
-        .learn-article-column__text{
-            display: grid;
-            gap: 26px;
-            padding: var(--f-gutter);
-        }
-
-        & .content {
-            width: 100%;
-
-            & .box {
-            padding: 0;
-            }
-        }
+    .learn-article-column__text {
+      display: grid;
+      gap: 26px;
+      padding: var(--f-gutter);
     }
 
-    &__paragraph-img{
+    & .content {
+      width: 100%;
 
-        @include respond-to("<=s") {
-            
-            .learn-article-column__text{
-                order: 1;
-            }
-        }
-        
-        .learn-article-column__text{
-            align-self: center;
-        }
+      & .box {
+        padding: 0;
+      }
     }
+  }
+
+  &__paragraph-img {
+    @include respond-to("<=s") {
+      .learn-article-column__text {
+        order: 1;
+      }
+    }
+
+    .learn-article-column__text {
+      align-self: center;
+    }
+  }
 }
 </style>
--- src/components/learn/LearnArticleHero.vue
+++ src/components/learn/LearnArticleHero.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="learn-article-hero">
-     
-          <img src="../../assets/learn-cover.jpg" alt="" class="learn-post__img" />
-
-    </div>
+    <img :src="featuredImgUrl" :alt="articleTitle" class="learn-post__img" />
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data: function () {
+    return {
+      featuredImgUrl: "img/learn-cover.jpg",
+      articleTitle: "Article title"
+    };
+  },
+};
 </script>
 
 <style lang="scss">
 @import "@lkmx/flare/src/functions/_respond-to.scss";
 
-.learn-article-hero{
-    padding:  42px;
+.learn-article-hero {
+  padding: 42px;
 
-    @include respond-to("<=s"){
-      padding: 0 0;
-    }
+  @include respond-to("<=s") {
+    padding: 0 0;
+  }
 }
 </style>
