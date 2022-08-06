063dfc88a589ed1312d3cd4675af138b6d56e7d7
Sandy Computer <the_dusky@sandysmacstudio.lan>
2022-08-05 20:15:43 -0400

    updated components

---
+++ src/components/dynamic/ArticleHero.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="article-hero">
+    <img :src="image.url" alt="" class="learn-post__img" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: Object
+  }
+}
+</script>
