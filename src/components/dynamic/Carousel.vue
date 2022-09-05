deb362c3e143c5cef6043212ff57fcd7a487d472
Walter Hurtado <walterhf@gmail.com>
2022-09-05 14:57:47 -0700

    wip carousel

--- src/components/dynamic/Carousel.vue
+++ src/components/dynamic/Carousel.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="mover">
-
-    <div class="items horizontal-slider learn-article__carouselItem">
-      <p>{{image}}</p>
+  <div class="mover learn-carousel">
+    <div class="learn-carousel__header">
+      <div class="learn-carousel__header__description">
+        <h3>{{ title }}</h3>
+        <p>{{ subtitle }}</p>
+      </div>
+      <div class="learn-carousel__header__controls">
+        <h3>{{ title }}</h3>
+        <p>{{ subtitle }}</p>
+      </div>
+    </div>
+    <div class="items horizontal-slider learn-carousel__item">
+      <p>{{ image }}</p>
       <div
         class="card-element item"
         v-for="(element, index) in articlesCarousel.data.learn_pages"
@@ -34,7 +43,7 @@
         </a>
       </div>
     </div>
-    
+
     <!-- </block>
     
     </column> -->
@@ -45,7 +54,9 @@
 export default {
   props: {
     dynamic_page_groups_learn_article: Object,
-    image: Object
+    title: String,
+    subtitle: String,
+    image: Object,
   },
 
   data() {
@@ -114,42 +125,124 @@ export default {
       this.appliedFilters = filters;
     },
     carouselItems() {
-      this.articlesCarousel = this.dynamic_page_groups_learn_article
-      this.tagCarousel = this.articlesCarousel.data.name
-      return this.articlesCarousel
+      this.articlesCarousel = this.dynamic_page_groups_learn_article;
+      this.tagCarousel = this.articlesCarousel.data.name;
+      return this.articlesCarousel;
     },
   },
 
   beforeMount() {
     this.carouselItems();
   },
-
-  
-
-  
 };
 </script>
 
-<static-query>
-  query {
-    coolStuff: allStrapiCoolStuff {
-      edges {
-        node {
-          id
-          title: name
-          url: link
-          description
-          cta_title
-          logo {
-            url
+<style lang="scss">
+.learn-carousel {
+  display: grid;
+  &__header {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    max-width: 1200px;
+    position: absolute;
+    background: lightcoral;
+    width: var(--f-breakpoint-xxl);
+  }
+  &__item {
+    white-space: nowrap;
+    display: grid;
+    grid-auto-flow: column;
+    justify-content: start;
+    padding-top: 100px;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+    .card-element {
+      border-radius: var(--f-radius);
+      overflow: hidden;
+      background: var(--theme-card-bg-default);
+      transition: 0.2s ease;
+      display: inline-flex;
+      text-align: center;
+      border-radius: 10px;
+      max-width: 400px;
+      min-width: 400px;
+
+      margin-right: var(--f-gutter-l);
+
+      white-space: normal;
+      justify-content: space-between;
+      vertical-align: top;
+      position: relative;
+      * {
+        margin: 0;
+      }
+      &__overall-link {
+        display: grid;
+        gap: 10px;
+        padding: var(--f-gutter);
+      }
+      &__header {
+        display: grid;
+        grid-template-columns: 1fr;
+        align-items: start;
+        gap: 4px;
+      }
+      &__title-desc {
+        display: grid;
+        gap: 8px;
+        text-align: left;
+        &__header {
+          display: grid;
+          gap: 0;
+          align-content: start;
+        }
+        .element-grid {
+          &-main-tag {
+            font-family: "Hind";
+            font-style: normal;
+            font-weight: 700;
+            font-size: 16px;
+            line-height: 24px;
+            color: #fbc998;
+            display: flex;
+            align-items: center;
+            letter-spacing: 1px;
+            text-transform: uppercase;
           }
-          types: type {
-            name
+          &-title {
+            color: white;
           }
+        }
+      }
+      &.hidden {
+        display: none;
+      }
+
+      img {
+        object-fit: cover;
+        width: 100%;
+        height: 206px;
+      }
+      &:hover {
+        background: var(--color-neutral-dark-mode-04);
+      }
+
+      * {
+        margin: 0;
+      }
+
+      h6 {
+        color: var(--theme-fg);
+      }
+
+      p {
+        &.tag {
+          text-transform: capitalize;
         }
       }
     }
   }
-</static-query>
-
-
+}
+</style>
--- src/templates/LearnSubpage.vue
+++ src/templates/LearnSubpage.vue
@@ -1,16 +1,136 @@
 <template>
   <default-layout class="learn-subpage">
-    <column class="spacer-s bg-black-gradient learn-subpage__content">
-      <div v-for="component in $context.components">
-
-          <block>
-            <component :is="component.comp_name" v-bind="component">
-              {{ component.content ? component.content : "" }}
-            </component>
-          </block>
-
-      </div>
+    <column
+      class="page-developers__cool-stuff horizontal-slider"
+      mode="full"
+      v-for="(component, index) in $context.components"
+      :key="index"
+    >
+      <block class="">
+        <component :is="component.comp_name" v-bind="component">
+          {{ component.content ? component.content : "" }}
+        </component>
+      </block>
+    </column>
+    <!-- Swirl bottom -->
+    <column class="blue__swirl__bottom" mode="full">
+      <block>
+        <img
+          class="get-scrt__align-img"
+          src="../../src/assets/swirl-orange-bottom.svg"
+        />
+      </block>
     </column>
   </default-layout>
 </template>
 
+<style lang="scss">
+@import "@lkmx/flare/src/functions/_respond-to.scss";
+.learn-subpage {
+  & .swirl-wrapper-bottom {
+    display: none;
+  }
+
+  & .simple-footer {
+    margin-top: 0;
+  }
+}
+
+// .learn-article {
+//   &__carouselItem {
+//     white-space: nowrap;
+//     display: grid;
+//     grid-auto-flow: column;
+//     justify-content: start;
+
+//     &::-webkit-scrollbar {
+//       display: none;
+//     }
+//     .card-element {
+//       border-radius: var(--f-radius);
+//       overflow: hidden;
+//       background: var(--theme-card-bg-default);
+//       transition: 0.2s ease;
+//       display: inline-flex;
+//       text-align: center;
+//       border-radius: 10px;
+//       max-width: 400px;
+//       min-width: 400px;
+
+//       margin-right: var(--f-gutter-l);
+
+//       white-space: normal;
+//       justify-content: space-between;
+//       vertical-align: top;
+//       position: relative;
+//       * {
+//         margin: 0;
+//       }
+//       &__overall-link {
+//         display: grid;
+//         gap: 10px;
+//         padding: var(--f-gutter);
+//       }
+//       &__header {
+//         display: grid;
+//         grid-template-columns: 1fr;
+//         align-items: start;
+//         gap: 4px;
+//       }
+//       &__title-desc {
+//         display: grid;
+//         gap: 8px;
+//         text-align: left;
+//         &__header {
+//           display: grid;
+//           gap: 0;
+//           align-content: start;
+//         }
+//         .element-grid {
+//           &-main-tag {
+//             font-family: "Hind";
+//             font-style: normal;
+//             font-weight: 700;
+//             font-size: 16px;
+//             line-height: 24px;
+//             color: #fbc998;
+//             display: flex;
+//             align-items: center;
+//             letter-spacing: 1px;
+//             text-transform: uppercase;
+//           }
+//           &-title {
+//             color: white;
+//           }
+//         }
+//       }
+//       &.hidden {
+//         display: none;
+//       }
+
+//       img {
+//         object-fit: cover;
+//         width: 100%;
+//         height: 206px;
+//       }
+//       &:hover {
+//         background: var(--color-neutral-dark-mode-04);
+//       }
+
+//       * {
+//         margin: 0;
+//       }
+
+//       h6 {
+//         color: var(--theme-fg);
+//       }
+
+//       p {
+//         &.tag {
+//           text-transform: capitalize;
+//         }
+//       }
+//     }
+//   }
+// }
+</style>
