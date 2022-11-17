10f207928e4ff8f783084b1aba9c0eb5ac8aaa79
Gildardo Diaz <gildardo.d@stakeordie.com>
2022-11-17 15:33:22 -0800

    feat(SNW-12): Implement Sponsors section

--- src/components/summit/BannerSection.vue
+++ src/components/summit/BannerSection.vue
@@ -22,47 +22,49 @@ export default {
 <style lang="scss">
 @import "@lkmx/flare/src/functions/respond-to";
 
-.summit_banner {
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 256px 26px;
-  background: url("../../assets/events/summit/background-glow.svg") center
-    no-repeat;
-  background-size: 150%;
+.summit_content {
+  .summit_banner {
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 256px 26px;
+    background: url("../../assets/events/summit/background-glow.svg") center
+      no-repeat;
+    background-size: 150%;
 
-  @include respond-to(">=m") {
-    background-size: 100%;
-  }
+    @include respond-to(">=m") {
+      background-size: 100%;
+    }
 
-  @include respond-to(">=l") {
-    background-size: contain;
-  }
+    @include respond-to(">=l") {
+      background-size: contain;
+    }
 
-  &__content {
-    div {
-      font-family: "Montserrat";
-      font-weight: 600;
-      line-height: 30px;
-      font-size: 20px;
-      text-align: center;
+    &__content {
+      div {
+        font-family: "Montserrat";
+        font-weight: 600;
+        line-height: 30px;
+        font-size: 20px;
+        text-align: center;
 
-      @include respond-to(">=s") {
-        line-height: 35px;
-        font-size: 32px;
-      }
-      @include respond-to(">=m") {
-        line-height: 45px;
-        font-size: 36px;
-      }
-      @include respond-to(">=l") {
-        line-height: 60px;
-        font-size: 54px;
-      }
+        @include respond-to(">=s") {
+          line-height: 35px;
+          font-size: 32px;
+        }
+        @include respond-to(">=m") {
+          line-height: 45px;
+          font-size: 36px;
+        }
+        @include respond-to(">=l") {
+          line-height: 60px;
+          font-size: 54px;
+        }
 
-      color: var(--color-analog-primary-white);
-      b {
-        color: var(--color-newBrand-blue-02);
+        color: var(--color-analog-primary-white);
+        b {
+          color: var(--color-newBrand-blue-02);
+        }
       }
     }
   }
--- src/components/summit/DescriptionSection.vue
+++ src/components/summit/DescriptionSection.vue
@@ -41,86 +41,88 @@ export default {
 <style lang="scss">
 @import "@lkmx/flare/src/functions/respond-to";
 
-.description {
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 26px;
-  padding: 136px 26px;
-
-  @include respond-to(">=m") {
-    padding: 136px 32px;
-  }
-
-  @include respond-to(">=l") {
-    padding: 136px 16px;
-    grid-template-columns: 342px 1fr;
-    gap: 42px;
-  }
-
-  &__info {
-    padding: 0;
-    @include respond-to(">=l") {
-      padding: 32px 16px;
-    }
-
-    &__subtitle {
-      color: var(--color-neutral-dark-mode-06);
-      font-weight: 400;
-      font-size: 18px;
-      font-family: Hind;
-      line-height: 30px;
-
-      @include respond-to(">=l") {
-        font-size: 20px;
-      }
-    }
-  }
-
-  &__cards {
+.summit_content {
+  .description {
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
     display: grid;
     grid-template-columns: 1fr;
-    gap: 42px 58px;
+    gap: 26px;
+    padding: 136px 26px;
 
     @include respond-to(">=m") {
-      grid-template-columns: repeat(2, 1fr);
+      padding: 136px 32px;
     }
 
     @include respond-to(">=l") {
-      padding-left: 42px;
-      border-left: 2px solid var(--color-neutral-gray-08);
+      padding: 136px 16px;
+      grid-template-columns: 342px 1fr;
+      gap: 42px;
     }
 
-    .card {
-      display: flex;
-      flex-direction: column;
-      gap: var(--f-gutter);
-
-      &__icon {
-        width: 48px;
-        height: 48px;
-        border-radius: 100%;
-        background-color: var(--color-newBrand-blue-03);
-        display: grid;
-        place-items: center;
-
-        img {
-          width: 24px;
-        }
+    &__info {
+      padding: 0;
+      @include respond-to(">=l") {
+        padding: 32px 16px;
       }
 
-      p {
-        margin: 0;
-        font-size: 18px;
-        color: var(--color-analog-primary-white);
-        font-family: Hind;
+      &__subtitle {
+        color: var(--color-neutral-dark-mode-06);
         font-weight: 400;
+        font-size: 18px;
+        font-family: Hind;
         line-height: 30px;
 
         @include respond-to(">=l") {
           font-size: 20px;
+        }
+      }
+    }
+
+    &__cards {
+      display: grid;
+      grid-template-columns: 1fr;
+      gap: 42px 58px;
+
+      @include respond-to(">=m") {
+        grid-template-columns: repeat(2, 1fr);
+      }
+
+      @include respond-to(">=l") {
+        padding-left: 42px;
+        border-left: 2px solid var(--color-neutral-gray-08);
+      }
+
+      .card {
+        display: flex;
+        flex-direction: column;
+        gap: var(--f-gutter);
+
+        &__icon {
+          width: 48px;
+          height: 48px;
+          border-radius: 100%;
+          background-color: var(--color-newBrand-blue-03);
+          display: grid;
+          place-items: center;
+
+          img {
+            width: 24px;
+          }
+        }
+
+        p {
+          margin: 0;
+          font-size: 18px;
+          color: var(--color-analog-primary-white);
+          font-family: Hind;
+          font-weight: 400;
+          line-height: 30px;
+
+          @include respond-to(">=l") {
+            font-size: 20px;
+          }
         }
       }
     }
