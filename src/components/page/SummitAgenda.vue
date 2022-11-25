ee04f1f6e6db9f5e2831302a607518fc1b024ef4
Gildardo Diaz <gildardo.d@stakeordie.com>
2022-11-25 15:15:48 -0800

    feat(SNW-28): Adjust color in UTC word

--- src/components/page/SummitAgenda.vue
+++ src/components/page/SummitAgenda.vue
@@ -389,6 +389,7 @@ query {
             @include small_text();
             font-size: 16px;
             text-transform: uppercase;
+            color: var(--color-neutral-gray-04);
           }
         }
       }
