d70cffe61dc302503a495ca2db9e6dff40fe3f13
Gildardo Diaz <gildardo.d@stakeordie.com>
2022-11-25 11:26:41 -0800

    feat(SNW-18): Integrate chat and live stream

--- src/components/summit/WelcomeSummit.vue
+++ src/components/summit/WelcomeSummit.vue
@@ -1,25 +1,56 @@
 <template>
   <div class="welcome">
-    <div class="welcome__content">
-      <div class="welcome__info">
-        <h4 class="welcome__info__eyebrow">Welcome to</h4>
-        <h2 class="welcome__info__title">Secret Summit 2022</h2>
-        <p>
-          Secret Network’s flagship digital conference with talks, panels, and
-          workshops by Web3 privacy pioneers from our ecosystem & beyond.
-        </p>
-      </div>
-      <a
-        href="/agenda"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="welcome__cta"
-      >
-        <span>VIEW FULL AGENDA</span>
-      </a>
-    </div>
+    <column>
+      <block>
+        <div class="welcome__content">
+          <div class="welcome__info">
+            <h4 class="welcome__info__eyebrow">{{ data.title }}</h4>
+            <h2 class="welcome__info__title">{{ data.subtitle }}</h2>
+            <p>{{ data.body }}</p>
+          </div>
+          <div class="livestream">
+            <iframe
+              class="livestream__video"
+              :src="liveStreamSrc"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+              allowfullscreen
+            ></iframe>
+            <iframe class="livestream__chat" :src="chatSrc"></iframe>
+          </div>
+          <a
+            :href="data.cta_button.url"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="welcome__cta"
+          >
+            <span>{{ data.cta_button.title }}</span>
+          </a>
+        </div>
+      </block>
+    </column>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    liveStreamSrc() {
+      return `https://www.youtube.com/embed/${this.data.livestream_code_id}?autoplay=1&mute=1`;
+    },
+    chatSrc() {
+      return `https://www.youtube.com/live_chat?v=${this.data.livestream_code_id}&embed_domain=${window.location.hostname}`;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import "@lkmx/flare/src/functions/respond-to";
@@ -27,27 +58,25 @@
 .summit_content {
   .welcome {
     margin-top: var(--header-height);
-    min-height: calc(100vh - var(--sum-heights));
     background-image: url("../../assets/events/summit/secret-summit-graphic-3-mobile.svg");
     background-position: left top;
     background-repeat: no-repeat;
     background-size: contain;
     transition: 0.2s ease;
-    display: flex;
-    /* align-items: center; */
 
     &__content {
       width: 100%;
       max-width: 1200px;
+      min-height: calc(100vh - var(--sum-heights));
       margin: 0 auto;
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
       gap: 26px;
-      padding: 0 26px;
+      padding: 26px;
 
-      @include respond-to(">=m") {
+      @include respond-to(">=l") {
         padding: 0;
       }
     }
@@ -123,6 +152,30 @@
         padding-top: 2px;
         font-weight: 700;
         letter-spacing: 1px;
+      }
+    }
+
+    .livestream {
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: 200px 350px;
+      place-items: stretch;
+      width: 100%;
+      gap: 26px;
+
+      @include respond-to(">=m") {
+        grid-template-rows: 350px 350px;
+      }
+
+      @include respond-to(">=l") {
+        grid-template-columns: minmax(max-content, 832px) 1fr;
+        grid-template-rows: 482px;
+      }
+
+      &__video {
+        @include respond-to(">=m") {
+          padding: 16px;
+        }
       }
     }
   }
