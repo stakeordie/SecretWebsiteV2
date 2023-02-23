fa9f95196ae39490ba67178cdab0668668e9d51a
Gildardo Diaz <gildardo.d@stakeordie.com>
2023-02-23 12:32:38 -0800

    fix(SNW-126): Fix columns on Grid component

---
+++ src/components/dynamic/cards/CardWrapper.vue
@@ -0,0 +1,28 @@
+<template>
+  <a
+    v-if="url"
+    :href="url"
+    :target="is_external_link ? '_blank' : ''"
+    :rel="is_external_link ? 'noopener noreferrer' : ''"
+  >
+    <slot></slot>
+  </a>
+  <article v-else>
+    <slot></slot>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: false
+    },
+    externalLink: {
+      type: Boolean,
+      required: false
+    }
+  }
+};
+</script>
