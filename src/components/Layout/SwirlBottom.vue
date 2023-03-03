0a4ce57bdebc048cfeb51a4f9218cdf420bb81ea
Gildardo Diaz <gildardo.d@stakeordie.com>
2023-03-02 16:38:08 -0800

    fix: Change swirl styles to scoped

--- src/components/Layout/SwirlBottom.vue
+++ src/components/Layout/SwirlBottom.vue
@@ -55,7 +55,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "@lkmx/flare/src/functions/respond-to";
 
 .swirl-wrapper {
--- src/components/Layout/SwirlTop.vue
+++ src/components/Layout/SwirlTop.vue
@@ -49,7 +49,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "@lkmx/flare/src/functions/respond-to";
 
 .swirl-wrapper {
