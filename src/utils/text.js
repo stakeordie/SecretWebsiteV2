384e774d3efb72562ad48c5dc85c2401ff5bdd28
Gildardo Diaz <100874861+gildardoSOD@users.noreply.github.com>
2023-03-22 16:06:19 -0700

    feat(SNW-155): Add Eventbrite option tu Cta Button component (#568)

--- src/utils/text.js
+++ src/utils/text.js
@@ -5,18 +5,16 @@ export const sizes = {
   large: "large",
 };
 
-export function replaceWithSpace(string = "") {
-  return string.replace(/[^A-Z0-9]+/ig, " ");
-}
+export const replaceWithSpace = (string = "") => string.replace(/[^A-Z0-9]+/ig, " ");
 
-export function removeCharacters(string = "") {
+export const removeCharacters = (string = "") => {
   return string
     .replace(/\s+/g, "-")
     .replace(/[^-A-Za-z0-9]+/g, "")
     .toLowerCase();
 }
 
-export function uppercaseAllFirstLetter(string = "") {
+export const uppercaseAllFirstLetter = (string = "") => {
   return string
     .split(" ")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
@@ -25,4 +23,20 @@ export function uppercaseAllFirstLetter(string = "") {
 
 export const removeLastSlash = (text = "") => {
   return text.charAt(text.length - 1) === "/" ? text.slice(0, -1) : text;
+};
+
+export const generateUUID = () => {
+  let uuid = "";
+  let random;
+
+  for (let i = 0; i < 32; i++) {
+    random = (Math.random() * 16) | 0;
+    if (i === 8 || i === 12 || i === 16 || i === 20) {
+      uuid += "-";
+    }
+    uuid += (i === 12 ? 4 : i === 16 ? (random & 3) | 8 : random).toString(
+      16
+    );
+  }
+  return uuid;
 };
