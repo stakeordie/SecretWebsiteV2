93c6960383708109a74f5b1586274d6710799903
Gildardo Diaz <100874861+gildardoSOD@users.noreply.github.com>
2023-04-03 17:51:03 -0700

    feat(SNW-166): Add option to open links in a new tab (#584)

--- src/components/dynamic/columns/TextColumnDouble.vue
+++ src/components/dynamic/columns/TextColumnDouble.vue
@@ -51,6 +51,7 @@
             :text="second_paragraph"
             :align="second_paragraph_alignment"
             :color="component_colors ? component_colors.body_color : ''"
+            :isExternalLink="is_external_link"
           />
           <DynamicButtons
             v-if="second_paragraph_buttons"
@@ -173,6 +174,11 @@ export default {
     component_colors: {
       type: Object,
       required: false
+    },
+    is_external_link: {
+      type: Boolean,
+      required: false,
+      default: false
     }
   },
   computed: {
